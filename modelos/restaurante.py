a49d68de470ad062926df12656b5b7425d185a19
GianCestarolli <giancestarolli@gmail.com>
2025-02-24 20:16:43 -0300

    calculando/exibindo avaliacoes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -15,9 +15,9 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print (f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}\n')
+        print (f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} |{"Status"}\n')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -32,3 +32,16 @@ class Restaurante:
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = 0
+        for avaliacao in self._avaliacao:
+            soma_das_notas += avaliacao._nota
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
+    
+        
