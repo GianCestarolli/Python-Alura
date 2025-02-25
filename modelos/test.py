28bac2d6bfdb033cebf4448627a2f5e6899ab9b0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-25 09:09:27 -0300

    metodo_estatico

--- modelos/test.py
+++ modelos/test.py
@@ -29,14 +29,12 @@ class Livro:
         return livros_disponiveis
                 
 
-                
-
-
+            
 livro_1984 = Livro("1984", "George Orwell", 1949)
 livro_noites_brancas = Livro("Noites Brancas", "Fiodor Dostoievsk", 1848)
-livro_1984.emprestar()
+livro_noites_brancas.emprestar()
 
-livros_disponiveis_ano = Livro.verificar_disponibilidade(1848)
+livros_disponiveis_ano = Livro.verificar_disponibilidade(1949)
 
 for livro in livros_disponiveis_ano:
     print(livro)
