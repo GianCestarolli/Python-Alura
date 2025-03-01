7c7af0d0b0b31ba9f06ffeb235a6ed360aa52e07
GianCestarolli <giancestarolli@gmail.com>
2025-03-01 17:57:52 -0300

    exibindo_cardapio

--- avancando_na_oo/main/modelos/restaurante.py
+++ avancando_na_oo/main/modelos/restaurante.py
@@ -1,6 +1,8 @@
 from modelos.avaliacao import Avaliacao
 from modelos.cardapio.item_cardapio import ItemCardapio
 
+
+
 class Restaurante:
     restaurantes = []
 
@@ -28,17 +30,21 @@ class Restaurante:
     def alternar_estado(self):
         self._ativo = not self._ativo
 
-    # def adicionar_bebida_no_cardapio(self, bebida):
-    #     self._cardapio.append(bebida)
-
-    # def adicionar_prato_no_cardapio(self, prato):
-    #     self._cardapio.append(prato)
-
     def adicionar_no_cardapio(self, item):
         if isinstance(item, ItemCardapio):
             self._cardapio.append(item)
 
-
+    @property
+    def exibir_cardapio(self):
+        print (f'Cardapio do restaurante {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start=1):
+            if hasattr(item, 'descricao'):
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
+                print(mensagem_bebida)
+                
     def receber_avaliacao(self, cliente, nota):
         if 0 < nota <= 5: 
             avaliacao = Avaliacao(cliente, nota)
@@ -52,3 +58,5 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
+    
+     
